25e9522b8ef884b8a70f1248944f08a616b03359
HarshaKy <harshaky6573@gmail.com>
2019-01-21 00:16:27 +0530

    more work

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const request = require('request');
+// const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode')
 
 const argv = yargs
   .options({
@@ -14,29 +16,10 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}%20+&key=AIzaSyCYJznXeBwok_h5AZ1QuvvMrtrkmcl1_yE`,
-  json: true
-}, (error, response, body) => {
-  // console.log(body);
-
-  if (error) {
-    console.log('unable to connect to google servers');
-  }
-
-    else if (body.status == 'ZERO_RESULTS') {
-    console.log('Unable to find location for that address');
-  }
-
-    else if (body.status == 'OVER_QUERY_LIMIT') {
-    console.log('You have exceeded your daily request quota for this API. If you did not set a custom daily request quota, verify your project has an active billing account: http://g.co/dev/maps-no-account');
-  }
-
-    else if (body.status == 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
   }
 });
---
+++ geocode/geocode.js
@@ -0,0 +1,44 @@
+const request = require('request');
+
+var geocodeAddress = (address, callback) => {
+  var encodedAddress = encodeURIComponent(address);
+
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}%20+&key=AIzaSyCYJznXeBwok_h5AZ1QuvvMrtrkmcl1_yE`,
+    json: true
+  }, (error, response, body) => {
+    // console.log(body);
+
+    if (error) {
+      callback('unable to connect to google servers');
+      // console.log('unable to connect to google servers');
+    }
+
+      else if (body.status == 'ZERO_RESULTS') {
+        callback('Unable to find location for that address');
+        // console.log('Unable to find location for that address');
+    }
+
+      else if (body.status == 'OVER_QUERY_LIMIT') {
+        callback('You have exceeded your daily request quota for this API.')
+        // console.log('You have exceeded your daily request quota for this API.');
+    }
+
+      else if (body.status == 'REQUEST_DENIED') {
+        callback('API key invalid/missing.')
+    }
+
+      else if (body.status == 'OK') {
+        callback(undefined, {
+          address: body.results[0].formatted_address,
+          latitude: body.results[0].geometry.location.lat,
+          longitude: body.results[0].geometry.location.lng
+        });
+        // console.log(`Address: ${body.results[0].formatted_address}`);
+        // console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        // console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
+  });
+};
+
+module.exports.geocodeAddress = geocodeAddress;
