ecd157284b4b5d16a26cdd4ce5edc726b569750c
HarshaKy <harshaky6573@gmail.com>
2019-01-21 00:40:58 +0530

    darksky.net to get weather

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
-// const request = require('request');
-const yargs = require('yargs');
+const request = require('request');
 
-const geocode = require('./geocode/geocode')
+// const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode')
+//
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Addr to fetch weather',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Addr to fetch weather',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
+// 8ddcf168b2f164ee9d3fffd5162a0be4
+// // example lat and lng
+// "latitude": 42.3601,
+// "longitude": -71.0589,
+
+request({
+  url:'https://api.darksky.net/forecast/8ddcf168b2f164ee9d3fffd5162a0be4/42.3601,-71.0589',
+  json: true
+} , (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log('unable to fetch weather');
   }
 });
